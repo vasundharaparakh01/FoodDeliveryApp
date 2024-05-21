ad65ed2eb87ec27afb5fd71692f13801d1664977
Vasundhara Parakh <52203447+vasundharaparakh01@users.noreply.github.com>
2024-05-21 16:23:15 +0530

    Delete Pods directory

--- Pods/Target Support Files/FAPanels/FAPanels-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FAPanels : NSObject
-@end
-@implementation PodsDummy_FAPanels
-@end
--- Pods/Target Support Files/TPKeyboardAvoidingSwift/TPKeyboardAvoidingSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TPKeyboardAvoidingSwift : NSObject
-@end
-@implementation PodsDummy_TPKeyboardAvoidingSwift
-@end
